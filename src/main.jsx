70bb16a9789dfab9a3e53de8a1947b78f2dc01dc
changhwan0411 <fani411@naver.com>
2026-06-12 14:40:32 +0900

    blog

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename=import.meta.env.BASE_URL{}>
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
